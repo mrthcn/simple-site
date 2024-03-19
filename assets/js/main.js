e2cf642599f3d30e9cd045196c9d2d604cf5ac12
mrthcn <catherinebuzhan@ukr.net>
2024-03-19 17:29:32 +0200

    add edit files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+$(document).ready(function(){
+	$('.navbar__btn').click(function(){
+		$(this).toggleClass('active');
+    $('.navbar__list').slideToggle(500);
+	});
+});
